44f15cfd7d48a6364833329ce971eeebac46302f
norah-st <113403393+norah-st@users.noreply.github.com>
2025-10-07 13:25:52 -0300

    example permissions route

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,7 +1,15 @@
 import { auth } from '$lib/auth';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async (event) => {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+
+	if (!session) redirect(307, '/login');
+	if (session.user.role !== 'admin') return error(403, 'No permission')
+
 	const users = await auth.api.listUserAccounts({
 		query: {
 			query: {
