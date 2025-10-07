f4dde68e19f97cfaf008ea15742b2904902a7b30
norah-st <113403393+norah-st@users.noreply.github.com>
2025-10-07 13:12:38 -0300

    formatting

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -2,16 +2,16 @@ import { auth } from '$lib/auth';
 import type { PageServerLoad } from './$types';
 
 export const load = (async (event) => {
-    const users = await auth.api.listUserAccounts({
-        query: {
-          query: {
-            searchValue: '',
-            searchOperator: 'contains'
-          }  
-        },
-        headers: event.request.headers
-    })
-    return {
-        users
-    };
+	const users = await auth.api.listUserAccounts({
+		query: {
+			query: {
+				searchValue: '',
+				searchOperator: 'contains'
+			}
+		},
+		headers: event.request.headers
+	});
+	return {
+		users
+	};
 }) satisfies PageServerLoad;
