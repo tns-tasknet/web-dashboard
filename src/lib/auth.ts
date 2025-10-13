1b110f2f1614fd83ce6555cd33511a17aaade5d5
norah-st <113403393+norah-st@users.noreply.github.com>
2025-10-12 21:35:31 -0300

    cors fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,25 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { sveltekitCookies } from "better-auth/svelte-kit";
-import { getRequestEvent } from "$app/server";
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { getRequestEvent } from '$app/server';
 
-import { PrismaClient } from "@prisma/client";
-import { admin, organization } from "better-auth/plugins";
-import { expo } from "@better-auth/expo";
+import { PrismaClient } from '@prisma/client';
+import { admin, organization } from 'better-auth/plugins';
+import { expo } from '@better-auth/expo';
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    emailAndPassword: {
-        enabled: true,
-    },
-    plugins: [expo(), admin(), organization(), sveltekitCookies(getRequestEvent)],
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql'
+	}),
+	emailAndPassword: {
+		enabled: true
+	},
+	trustedOrigins: [
+		'http://localhost:8081',
+		'http://localhost',
+		'https://prod-tns-tasknet-usm.vercel.app'
+	],
+	plugins: [expo(), admin(), organization(), sveltekitCookies(getRequestEvent)]
 });
