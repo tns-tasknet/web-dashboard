a32476580d98925ff495763b65091976876bec67
norah-st <113403393+norah-st@users.noreply.github.com>
2025-10-22 02:42:58 -0300

    return reports on api

---
+++ src/routes/api/v1/[organizationSlug]/reports/+server.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from './$types';
+import { auth } from '$lib/auth';
+import { prisma } from '$lib/server/prisma';
+import { json } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async (event) => {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+
+	if (!session) return new Response(null, { status: 401 });
+
+	const member = await auth.api.getActiveMember({
+		headers: event.request.headers
+	});
+
+	const reports = await prisma.report.findMany({
+		where: {
+			organization: {
+				slug: event.params.organizationSlug
+			},
+			memberId: member?.id
+		}
+	});
+
+	return json(reports);
+};
