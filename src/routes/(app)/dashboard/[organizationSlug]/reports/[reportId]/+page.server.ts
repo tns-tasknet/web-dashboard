f7ef87b2218ffc1965bc3dc4d9d1c2680b3cad1e
norah-st <113403393+norah-st@users.noreply.github.com>
2025-11-06 20:50:23 -0300

    messages backend

--- src/routes/(app)/dashboard/[organizationSlug]/reports/[reportId]/+page.server.ts
+++ src/routes/(app)/dashboard/[organizationSlug]/reports/[reportId]/+page.server.ts
@@ -1,5 +1,5 @@
 import { auth } from '$lib/auth';
-import { error, redirect } from '@sveltejs/kit';
+import { error, redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 
@@ -29,6 +29,28 @@ export const load = (async (event) => {
 				slug: event.params.organizationSlug
 			},
 			id: Number(event.params.reportId)
+		},
+		include: {
+			messages: {
+				include: {
+					sender: {
+						include: {
+							user: {
+								select: {
+									id: true,
+									name: true,
+									email: true,
+									image: true,
+									role: true
+								}
+							}
+						}
+					}
+				},
+				orderBy: {
+					createdAt: 'desc'
+				}
+			}
 		}
 	});
 
@@ -39,3 +61,53 @@ export const load = (async (event) => {
 		report
 	};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	default: async (event) => {
+		const session = await auth.api.getSession({
+			headers: event.request.headers
+		});
+
+		if (!session) redirect(307, '/login');
+
+		await auth.api.setActiveOrganization({
+			body: {
+				organizationSlug: event.params.organizationSlug
+			},
+			headers: event.request.headers
+		});
+
+		const member = await auth.api.getActiveMember({
+			headers: event.request.headers
+		});
+
+		if (isNaN(Number(event.params.reportId))) error(400, 'Hello World');
+
+		const report = await prisma.report.findFirst({
+			where: {
+				id: Number(event.params.reportId)
+			}
+		});
+
+		if (!report) error(404);
+		// TODO: Add form validation
+		/*
+		const form = await superValidate(event, formSchema);
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+		*/
+		const data = await event.request.formData();
+
+		await prisma.message.create({
+			data: {
+				content: data.get('message')!.toString(),
+				memberId: member!.id,
+				reportId: report.id
+			}
+		});
+		return {};
+	}
+};
--- src/routes/(app)/dashboard/[organizationSlug]/reports/[reportId]/+page.svelte
+++ src/routes/(app)/dashboard/[organizationSlug]/reports/[reportId]/+page.svelte
@@ -17,3 +17,12 @@ Assign it to an user
 
 <h1>REPORT DETAILS</h1>
 <pre>{JSON.stringify(data, null, 4)}</pre>
+
+<form method="POST">
+	<fieldset class="fieldset">
+		<legend class="fieldset-legend">create message</legend>
+		<input type="text" placeholder="Type here" class="input" name="message" />
+	</fieldset>
+
+	<button type="submit" class="btn btn-primary">Create message</button>
+</form>
